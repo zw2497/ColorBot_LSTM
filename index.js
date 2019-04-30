3e242919558941ae5b27b3d6c026fbd9e8d6c51e
zw2497 <zhicheng.wu@columbia.edu>
2019-04-30 15:16:26 +0000

    colab -> github

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ class Classifier {
       //console.log(word, this.wordIndex[word], inputBuffer);
     }
     const input = inputBuffer.toTensor();
-    //console.log(input);
+    console.log(input);
 
     status('Running inference');
     const beginMs = performance.now();
